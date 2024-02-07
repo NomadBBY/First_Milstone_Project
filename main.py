58bb05619af5431cf7749966c459319c4c9207ac
Artis_Code <89861525+NomadBBY@users.noreply.github.com>
2024-02-07 13:14:34 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+grid = [[" ", " ", " "], 
+        [" ", " ", " "], 
+        [" ", " ", " "]]
+
+for number in grid:
+    print(number)
+
+def welcome_message(name):
+    pass
+
+def player_picking():
+    pass
+
+def display_game_state():
+    pass
+
+
